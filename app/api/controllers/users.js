69d80f4b109dc58af467da07df5fd30cda81772b
kmtc24 <kmtc241@gmail.com>
2020-04-08 02:15:15 +0900

    admin login 수정

--- app/api/controllers/users.js
+++ app/api/controllers/users.js
@@ -14,16 +14,17 @@ module.exports = {
     },
     authenticate: function (req, res, next) {
         userModel.findOne({ email: req.body.email }, function (err, userInfo) {
-            if (err) {
-                next(err);
+            if (userInfo == undefined) {
+                res.json({ status: "fail", message: "등록되지 않은 어드민 계정입니다." });
             } else {
                 if (bcrypt.compareSync(req.body.password, userInfo.password)) {
-                    const token = jwt.sign({ id: userInfo._id }, req.app.get('secretKey') ,{ expiresIn: '1h' });
+                    const token = jwt.sign({ id: userInfo._id }, req.app.get('secretKey'), { expiresIn: '1h' });
                     res.json({ status: "success", message: "토큰 발행, 로그인 성공", data: { user: userInfo, token: token } });
                 } else {
                     res.json({ status: "error", message: "옳바르지 않은 아이디 및 비밀번호입니다.", data: null });
                 }
             }
+
         });
     },
 }
