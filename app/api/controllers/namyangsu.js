ce07b2c87088b66c46af824b0c2597fca5df0a96
kmtc24 <kmtc241@gmail.com>
2020-04-08 00:38:06 +0900

    count 제한

--- app/api/controllers/namyangsu.js
+++ app/api/controllers/namyangsu.js
@@ -35,16 +35,20 @@ module.exports = {
     
 
     updateById: function (req, res, next) {
+        
         const query = {phone: req.body.phone};
         namyangsuModel.findOne(query, function(err, userInfo){
             if(err){
                 next(err);
             }else{
-                var updatedCount = userInfo.count + 1;
-                namyangsuModel.findOneAndUpdate(query, {count: updatedCount }, function(err, userInfo){
-                        res.json({status:"Success", message: "마스크 보급을 완료하였습니다.", data:null});
-        
-                });
+                if(userInfo.count > 1){
+                    res.json({status: "fail", message:"마스크 지급 회수를 초과하였습니다.", data:userInfo});
+                } else{
+                    var updatedCount = userInfo.count + 1;
+                    namyangsuModel.findOneAndUpdate(query, {count: updatedCount}, function(err, userInfo){
+                            res.json({status:"Success", message: "마스크 보급을 완료하였습니다.", data:userInfo});
+                        });
+                }                
             }
         })
    
