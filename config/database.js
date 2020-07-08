d032665a96b5c5e29c50ee57b89c19a40a64f50e
kmtc24 <kmtc241@gmail.com>
2020-07-08 13:17:21 +0900

    mongodb revision

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // MongoDB 연결
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb+srv://venduster:qpsej0424!@cluster0-on6pa.mongodb.net/test?retryWrites=true&w=majority';
-mongoose.connect(mongoDB);
+const mongoDB = 'mongodb://venduster:qpsej0424!@cluster0-shard-00-00-on6pa.mongodb.net:27017,cluster0-shard-00-01-on6pa.mongodb.net:27017,cluster0-shard-00-02-on6pa.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
